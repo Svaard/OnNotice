be1a68dcc67dd8e712095025554b15f9ce5587f3
svaard <daniel.n.flores@gmail.com>
2021-05-11 18:50:31 -0700

    Add voice alerts.

--- notice.py
+++ notice.py
@@ -3,3 +3,4 @@ import subprocess
 title = "Test"
 message = "test"
 subprocess.run(['osascript', '-e', f'display notification "{message}" with title "{title}"'])
+subprocess.run(['osascript', '-e', f'say "{message}"'])
